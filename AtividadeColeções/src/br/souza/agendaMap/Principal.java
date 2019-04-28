b6241c43ea4681aa75c17da72dcb21847907e186
Brendo Souza Pinheiro <brendo.pinheiro@ucsal.edu.br>
2019-04-28 17:23:11 -0300

    Atualização da agenda

--- AtividadeColeções/src/br/souza/agendaMap/Principal.java
+++ AtividadeColeções/src/br/souza/agendaMap/Principal.java
@@ -1,69 +1,91 @@
 package br.souza.agendaMap;
 
-import java.util.Scanner;
-
 import javax.swing.JOptionPane;
+import javax.swing.JTextField;
 
 public class Principal {
-
 	public static void main(String[] args) {
 		Agenda agenda = new Agenda();
+		agendaEletronica(agenda);
+	}
+	public static void agendaEletronica(Agenda agenda) {
+		JTextField nomeField = new JTextField(10);
+        JTextField telefoneField = new JTextField(5);
+        JTextField descricaoField =new JTextField();
+
+		Object[]  addContato = {
+                "Nome:",nomeField,
+                "Telefone:",telefoneField,
+                "Descrição:", descricaoField,
+        };
+		Object[]  buscarContato = {
+                "Nome:",nomeField, 
+        };
 		
-		Scanner input = new Scanner(System.in);
-		String nome = "", telefone = "", descricao = "", continuacao = "Deseja continuar na Agenda? \n(1) - Continuar, (2) - Sair";
-		int continuar = 0;
-		
-		JOptionPane.showMessageDialog(null, "Bem-vindo a sua Agenda eletrônica!");
+		JOptionPane.showMessageDialog(null, "Bem-vindo a sua Agenda eletrônica!", "Agenda Eletrônica", JOptionPane.PLAIN_MESSAGE);
+
+		String msgContinua = "Deseja continuar na agenda?";
+		int continuar = -1, resultado = 2;
 		
 		do {
-			int decisao = Integer.parseInt(JOptionPane.showInputDialog("Qual operação deseja realizar? "
-					+ "\n(1) - Adicionar Contatos, (2) - Buscar Contatos, (3) - Remover Contatos, (4) - Abrir Agenda, (5) - Sair: "));
-			
-			
+			int decisao = Integer.parseInt(JOptionPane.showInputDialog(null, "Qual operação deseja realizar? "
+					+ "\n(1) - Adicionar Contatos\n(2) - Buscar Contatos"
+					+ "\n(3) - Remover Contatos\n(4) - Abrir Agenda\n(5) - Sair: ", "Opções", JOptionPane.QUESTION_MESSAGE));
 			
 			switch(decisao){
 				case 1:
-					JOptionPane.showMessageDialog(null, "Você está na área de Adicionar Contatos!");
-					nome = JOptionPane.showInputDialog("Informe o nome do Contato: ");
-					telefone = JOptionPane.showInputDialog("Informe o número do Contato: ");
-					descricao = JOptionPane.showInputDialog("Informe a descrição do Contato: ");
+					JOptionPane.showMessageDialog(null, "Você está na área de Adicionar Contatos!", "Adicionar Contato", JOptionPane.PLAIN_MESSAGE);
 					
-					agenda.addContatos(nome, telefone, descricao);
-					
-					continuar = Integer.parseInt(JOptionPane.showInputDialog(continuacao));
+					resultado = JOptionPane.showConfirmDialog(null, addContato,
+			                "Adicionar contato", JOptionPane.OK_CANCEL_OPTION);
+					saida(resultado);
+					 
+					agenda.addContatos(nomeField.getText(), telefoneField.getText(), descricaoField.getText());
+					nomeField.setText("");
+					telefoneField.setText("");
+					descricaoField.setText("");
+
+					continuar = JOptionPane.showConfirmDialog(null,msgContinua);
 					break;
 					
 				case 2:
-					JOptionPane.showMessageDialog(null, "Você está na área de Busca de Contatos!");
-					nome = JOptionPane.showInputDialog("Informe o nome do Contato que deseja buscar: ");
-					JOptionPane.showMessageDialog(null,agenda.buscarContatos(nome));
-					
-					continuar = Integer.parseInt(JOptionPane.showInputDialog(continuacao));
+					JOptionPane.showMessageDialog(null, "Você está na área de Busca de Contatos!", "Busca de Contatos", JOptionPane.INFORMATION_MESSAGE);
+					resultado = JOptionPane.showConfirmDialog(null, buscarContato,
+			                "Busca de Contato", JOptionPane.OK_CANCEL_OPTION);
+					saida(resultado);
+					JOptionPane.showMessageDialog(null,agenda.buscarContatos(nomeField.getText()));
+
+					continuar = JOptionPane.showConfirmDialog(null,msgContinua);
 					break;
-					
 				case 3:
 					JOptionPane.showMessageDialog(null, "Você está na área de Remoção Contatos!");
-					nome = JOptionPane.showInputDialog("Informe o nome do Contato que deseja remover: ");
+					resultado = JOptionPane.showConfirmDialog(null, buscarContato,
+	                "Busca de Contato", JOptionPane.OK_CANCEL_OPTION);
+					saida(resultado);
 					
-					agenda.removerContatos(nome);
+					agenda.removerContatos(nomeField.getText());
 					
-					continuar = Integer.parseInt(JOptionPane.showInputDialog(continuacao));
+					continuar = JOptionPane.showConfirmDialog(null,msgContinua);
 					break;
 					
 				case 4:
 					JOptionPane.showMessageDialog(null, "Esta é a sua agenda!");
 					JOptionPane.showMessageDialog(null,agenda);
-					continuar = Integer.parseInt(JOptionPane.showInputDialog(continuacao));
+					continuar = JOptionPane.showConfirmDialog(null,msgContinua);
 					break;
 					
 				case 5:
 					JOptionPane.showMessageDialog(null, "Saindo!");
 					break;
 			}
-		}while(continuar == 1);
+		}while(continuar == 0);
 		
-		input.close();
-		}
+	}
+	public static void saida(int resultado) {
+		if (resultado == JOptionPane.CANCEL_OPTION) {
+            System.exit(0);
+        }
+	}
 }
 
 
